904c2cafd31ea0b8c0ea9cf5856f65d18b3d88dd
Licheus <Licheus@hotmail.com>
2016-01-09 18:15:42 +0100

    Product tab sections hide/display
    Updated product tab sections to hide/display properly. Also updated page Product.aspx to match.

--- Project/Scripts/Product.js
+++ Project/Scripts/Product.js
@@ -1,30 +1,29 @@
-﻿window.onload = function()
+window.onload = function()
 {
     /* Give all the tabs onclick-events */
-    document.getElementById("productsection_tab_product").onclick = productSectionTabClick;
-    document.getElementById("productsection_tab_content").onclick = productSectionTabClick;
-    document.getElementById("productsection_tab_rating").onclick = productSectionTabClick;
-    document.getElementById("productsection_tab_sysreq").onclick = productSectionTabClick;
-    document.getElementById("productsection_tab_share").onclick = productSectionTabClick;
+    document.getElementById("productsection_tab_product").onclick = tabClick;
+    document.getElementById("productsection_tab_content").onclick = tabClick;
+    document.getElementById("productsection_tab_rating").onclick = tabClick;
+    document.getElementById("productsection_tab_sysreq").onclick = tabClick;
+    document.getElementById("productsection_tab_share").onclick = tabClick;
 
     /* Give all the tabs mouseover-events */
-    document.getElementById("productsection_tab_product").onmouseover = productSectionTabOver;
-    document.getElementById("productsection_tab_content").onmouseover = productSectionTabOver;
-    document.getElementById("productsection_tab_rating").onmouseover = productSectionTabOver;
-    document.getElementById("productsection_tab_sysreq").onmouseover = productSectionTabOver;
-    document.getElementById("productsection_tab_share").onmouseover = productSectionTabOver;
+    document.getElementById("productsection_tab_product").onmouseover = tabHover;
+    document.getElementById("productsection_tab_content").onmouseover = tabHover;
+    document.getElementById("productsection_tab_rating").onmouseover = tabHover;
+    document.getElementById("productsection_tab_sysreq").onmouseover = tabHover;
+    document.getElementById("productsection_tab_share").onmouseover = tabHover;
 
     /* Give all the tabs mouseout-events */
-    document.getElementById("productsection_tab_product").onmouseout = productSectionTabReset;
-    document.getElementById("productsection_tab_content").onmouseout = productSectionTabReset;
-    document.getElementById("productsection_tab_rating").onmouseout = productSectionTabReset;
-    document.getElementById("productsection_tab_sysreq").onmouseout = productSectionTabReset;
-    document.getElementById("productsection_tab_share").onmouseout = productSectionTabReset;
+    document.getElementById("productsection_tab_product").onmouseout = tabReset;
+    document.getElementById("productsection_tab_content").onmouseout = tabReset;
+    document.getElementById("productsection_tab_rating").onmouseout = tabReset;
+    document.getElementById("productsection_tab_sysreq").onmouseout = tabReset;
+    document.getElementById("productsection_tab_share").onmouseout = tabReset;
 }
 
-
 /* OnClick */
-function productSectionTabClick()
+function tabClick()
 {
     /* Unselect currently selected tab */
     var tabs = document.getElementsByClassName("productsection_tab_design productsection_tab_text productsection_tab_selected");
@@ -36,12 +35,39 @@ function productSectionTabClick()
         /* Set this tab as selected */
         this.setAttribute("class", "productsection_tab_design productsection_tab_text productsection_tab_selected");
 
-        /* TODO: Change what text is fetched from the database in the productsection_contenttextcell */
+        /* Hide all sections */
+        sectionsHide();
+
+        /* Display selected section */
+        switch (this.id)
+        {
+            case "productsection_tab_product":
+                document.getElementById("general_product_logoimg").style.display = "inherit";
+                document.getElementById("general_product_title").style.display = "inherit";
+                document.getElementById("product_section_text").style.display = "inherit";
+                break;
+            case "productsection_tab_content":
+                document.getElementById("general_product_logoimg").style.display = "inherit";
+                document.getElementById("general_product_title").style.display = "inherit";
+                document.getElementById("content_section_text").style.display = "inherit";
+                break;
+            case "productsection_tab_rating":
+                document.getElementById("general_product_rating").style.display = "inherit";
+                break;
+            case "productsection_tab_sysreq":
+                document.getElementById("general_product_logoimg").style.display = "inherit";
+                document.getElementById("general_product_title").style.display = "inherit";
+                document.getElementById("sysreq_section_text").style.display = "inherit";
+                break;
+            case "productsection_tab_share":
+                document.getElementById("general_product_share").style.display = "inherit";
+                break;
+        }
     }
 }
 
 /* OnMouseOver */
-function productSectionTabOver()
+function tabHover()
 {
     /* Change unselected tabs only */
     if (this.className == "productsection_tab_design productsection_tab_text productsection_tab_unselected")
@@ -52,7 +78,7 @@ function productSectionTabOver()
 }
 
 /* OnMouseOut */
-function productSectionTabReset()
+function tabReset()
 {
     /* Change hovered tabs only */
     if (this.className == "productsection_tab_design productsection_tab_text productsection_tab_hover")
@@ -60,4 +86,18 @@ function productSectionTabReset()
         /* Reset class/unselect tab */
         this.setAttribute("class", "productsection_tab_design productsection_tab_text productsection_tab_unselected");
     }
+}
+
+/* Hide all sections */
+function sectionsHide()
+{
+    document.getElementById("general_product_rating").style.display = "none";
+    document.getElementById("general_product_share").style.display = "none";
+
+    document.getElementById("general_product_logoimg").style.display = "none";
+    document.getElementById("general_product_title").style.display = "none";
+    
+    document.getElementById("product_section_text").style.display = "none";
+    document.getElementById("content_section_text").style.display = "none";
+    document.getElementById("sysreq_section_text").style.display = "none";
 }
